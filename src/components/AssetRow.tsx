e0f2ed57810b6cc48f1b87528ed4d8f0afa3dd1a
mucahitcetin <mucahitcetin01@gmail.com>
2024-07-05 20:03:23 +0300

    style edited

--- src/components/AssetRow.tsx
+++ src/components/AssetRow.tsx
@@ -4,6 +4,7 @@ import SparklineChart from "./SparklineChart";
 import millify from "millify";
 import formatNumber from "../utils/formatNumber";
 import { Asset } from "../types";
+import { GoArrowDownRight, GoArrowUpRight } from "react-icons/go";
 
 interface AssetRowProps {
   asset: Asset;
@@ -35,6 +36,18 @@ const AssetRow: React.FC<AssetRowProps> = ({ asset }) => {
     return () => unsubscribe();
   }, [asset.symbol, price]);
 
+  const getColorClass = (change: number) => {
+    if (change > 0) return "text-green-500";
+    if (change < 0) return "text-red-500";
+    return "text-gray-500";
+  };
+
+  const getArrowIcon = (change: number) => {
+    if (change > 0) return <GoArrowUpRight className="inline mr-1" />;
+    if (change < 0) return <GoArrowDownRight className="inline mr-1" />;
+    return null;
+  };
+
   return (
     <tr>
       <td className="py-4">
@@ -43,20 +56,17 @@ const AssetRow: React.FC<AssetRowProps> = ({ asset }) => {
           / USDT
         </span>
       </td>
-      <td className={`ps-5 py-2 text-right ${priceClass}`}>
+      <td className={`ps-5 py-2 text-right font-semibold ${priceClass}`}>
         {formatNumber(price)}
         <Symbol />
       </td>
-      <td className="py-2 text-right">
+      <td className="py-2 text-right font-semibold">
         {millify(+marketValue)}
         <Symbol />
       </td>
-      <td
-        className={`py-2 ${
-          +change > 0 ? "text-green-500" : "text-red-500"
-        } text-right`}
-      >
-        {parseFloat(Number(change).toFixed(2))}%
+      <td className={`py-2 font-semibold ${getColorClass(+change)} text-right`}>
+        {getArrowIcon(+change)}
+        {Math.abs(parseFloat(Number(change).toFixed(2)))}%
       </td>
       <td className="ps-4 py-2">
         <SparklineChart data={sparklineData} />
